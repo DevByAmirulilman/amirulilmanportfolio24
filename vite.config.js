3d4cf966f65b5b6e6a765010f5473adb1ed49be3
Amirul ilman <ilman1amirul@gmail.com>
2024-09-19 14:20:55 +0800

    test3

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { transformWithEsbuild } from 'vite'
-import restart from 'vite-plugin-restart'
+import react from '@vitejs/plugin-react';
+import { transformWithEsbuild } from 'vite';
+import restart from 'vite-plugin-restart';
 
-export default {
+export default ({ mode }) => ({
+    base: '/amirulilmanportfolio24/',
     root: 'src/',
     publicDir: '../public/',
-    plugins:
-    [
+    plugins: [
         // Restart server on static/public file change
-        restart({ restart: [ '../public/**', ] }),
+        restart({ restart: ['../public/**'] }),
 
         // React support
         react(),
@@ -16,11 +16,8 @@ export default {
         // .js file support as if it was JSX
         {
             name: 'load+transform-js-files-as-jsx',
-            async transform(code, id)
-            {
-                if (!id.match(/src\/.*\.js$/))
-                    return null
-
+            async transform(code, id) {
+                if (!id.match(/src\/.*\.js$/)) return null;
                 return transformWithEsbuild(code, id, {
                     loader: 'jsx',
                     jsx: 'automatic',
@@ -28,15 +25,22 @@ export default {
             },
         },
     ],
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    server: {
+        host: true, // Open to local network
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open browser unless in sandbox
     },
-    build:
-    {
+    build: {
         outDir: '../dist', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: mode === 'development', // Enable sourcemaps only in development
+        chunkSizeWarningLimit: 500, // Default chunk size warning limit
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    // Chunk splitting for vendor libraries
+                    vendor: ['react', 'react-dom', 'three', '@mui/material'],
+                },
+            },
+        },
     },
-}
+});
